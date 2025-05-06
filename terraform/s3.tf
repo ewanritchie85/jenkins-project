d8975fc5c286c9afc335e0568f5eb7f39b6c5739
Ewan Ritchie <ewan.ritchie@ten10.com>
2025-05-06 16:55:57 +0100

    terraform s3 tidied up

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -25,8 +25,6 @@ resource "aws_s3_bucket_public_access_block" "frontend_block" {
 
   block_public_acls       = false
   block_public_policy     = false
-#   ignore_public_acls      = false
-#   restrict_public_buckets = false
 }
 
 
