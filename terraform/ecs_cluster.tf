015b0cfdefe43dd1ef9c1b028cc3f3b8adc225af
Ewan Ritchie <ewan.ritchie@ten10.com>
2025-05-06 11:18:18 +0100

    ALB listener port and target group changed from port 3000 to port 80

--- terraform/ecs_cluster.tf
+++ terraform/ecs_cluster.tf
@@ -9,7 +9,7 @@ resource "aws_ecs_cluster" "ecs_cluster" {
 resource "aws_lb_target_group" "alb_target_group" {
   name        = var.alb_target_group_name
   target_type = "ip"
-  port        = 3000
+  port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.vpc.id
   tags = {
@@ -18,7 +18,7 @@ resource "aws_lb_target_group" "alb_target_group" {
 
 resource "aws_lb_listener" "alb_listener" {
   load_balancer_arn = aws_lb.alb.arn
-  port              = 3000
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
